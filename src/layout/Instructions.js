4b587fd239671f8d2cf14ce680af1b1d95381d16
Tyler Casperson <tylercasperson@gmail.com>
2021-01-16 19:03:30 -0700

    updated instructions

--- src/layout/Instructions.js
+++ src/layout/Instructions.js
@@ -2,10 +2,23 @@ import React from 'react';
 
 const Instructions = () => {
   return (
-    <div>
-      Enter a number for each side. As soon as all three fields have valid data
-      in them, calculations will run to let you know information about the
-      triangle.
+    <div
+      style={{
+        height: '5vh',
+        display: 'flex',
+        justifyContent: 'center',
+      }}
+    >
+      <div
+        style={{
+          width: '40vw',
+          textAlign: 'center',
+        }}
+      >
+        Enter a number (natural or decimal) for each side. As soon as all three
+        fields have valid data in them, calculations will run to let you know
+        information about the triangle.
+      </div>
     </div>
   );
 };
